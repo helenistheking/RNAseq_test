0b5ee84d69232815225762b2fae7d423d76e3e2d
helenistheking <heking@ucsd.edu>
2018-12-28 17:16:25 +0000

    First commit

---
+++ app.R
@@ -0,0 +1,5 @@
+# install.packages("ssh")
+library("ssh", lib.loc="/Library/Frameworks/R.framework/Versions/3.4/Resources/library")
+
+ssh_connect("k1632479@login.rosalind.kcl.ac.uk", passwd="Hkdogs222*")
+
