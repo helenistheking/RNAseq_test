929bb6e5898c7eb6f8caf5beacb25683d39d8db5
helenistheking <36276388+helenistheking@users.noreply.github.com>
2019-01-09 16:19:29 +0000

    Update setup.R

--- setup.R
+++ setup.R
@@ -2,7 +2,7 @@
 install.packages("ssh")
 library("ssh", lib.loc="/Library/Frameworks/R.framework/Versions/3.4/Resources/library")
 
-session <- ssh_connect("k1632479@login.rosalind.kcl.ac.uk", passwd="Hkdogs222*")
+session <- ssh_connect("k1632479@login.rosalind.kcl.ac.uk", passwd=)
 
 
 # loading out of date packages 
